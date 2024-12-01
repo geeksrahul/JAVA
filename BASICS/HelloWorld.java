9c9fa5e16d8a14401229a94739a50c459ff5d9d2
geeksrahul <geeksrahulofficial@gmail.com>
2024-12-01 21:19:53 +0530

    edit HelloWorld program

--- BASICS/HelloWorld.java
+++ BASICS/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld { 
     public static void main(String[] args) {
-        System.out.println("Hello World !"); // print Hello World and add line break 
-        System.out.print("Hello World !"); // print Hello World only
+        System.out.println("Hello World !"); // print Hello World 
     }
 }
