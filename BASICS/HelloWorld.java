93e148dc21ae5e25273cd338652409b2b6b593b3
geeksrahul <geeksrahulofficial@gmail.com>
2024-12-01 21:13:41 +0530

    create first java program !

---
+++ BASICS/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld { 
+    public static void main(String[] args) {
+        System.out.println("Hello World !"); // print Hello World and add line break 
+        System.out.print("Hello World !"); // print Hello World only
+    }
+}
