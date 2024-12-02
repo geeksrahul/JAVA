40df30f99b98a6af4d15b345a0bf4262bd2a1956
geeksrahul <geeksrahulofficial@gmail.com>
2024-12-02 21:50:09 +0530

    looping statement programs

---
+++ BASICS/DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int i =0;
+        while(i!=0 && i<3) {
+            System.out.println("while loop");
+            i++;
+        }
+        do { 
+            System.out.println("do while loop");
+            i++;
+        } while (i!=0 && i<3);
+    }
+}
---
+++ BASICS/ForEachLoop.java
@@ -0,0 +1,12 @@
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr;
+        arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i : arr ) {
+            System.out.println(i);
+        }
+    }
+}
