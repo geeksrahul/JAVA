c6fb101bc821c4207c1a0aa6aca6b2617310a39c
geeksrahul <geeksrahulofficial@gmail.com>
2024-12-09 22:27:03 +0530

    add varargs

---
+++ BASICS/p21_VarArgs.java
@@ -0,0 +1,24 @@
+public class p21_VarArgs {
+    // public static int calculateSum(int num1, int num2) {
+    //     int sum = num1 + num2;
+    //     return sum;
+    // }
+    // public static int calculateSum(int num1, int num2,int num3) {
+    //     int sum = num1 + num2 + num3;
+    //     return sum;
+    // }
+    // public static int calculateSum(int num1, int num2,int num3, int num4) {
+    //     int sum = num1 + num2 + num3 + num4;
+    //     return sum;
+    // }
+    public static int calculateSum(int num1,int ...arr){
+        int sum = num1;
+        for(int i=0; i<arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(calculateSum(1,10,20,30));
+    }
+}
