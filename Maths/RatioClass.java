00d2c166ff8c19515b74d69aa64f5f1a2166ef49
geeksrahul <geeksrahulofficial@gmail.com>
2024-12-12 23:10:00 +0530

    12-12-2024

---
+++ Maths/RatioClass.java
@@ -0,0 +1,13 @@
+class Ratio {
+    int numerator;
+    int denominator;
+    public Ratio(int n, int d){
+        this.numerator = n;
+        this.denominator = d;
+    }
+}
+public class RatioClass {
+    public static void main(String[] args) {
+        
+    }
+}
